c9eaa81275af2d60d2d4cd74a2d769c62651dd73
Deep Muni <deep.muni94@gmail.com>
2020-09-23 23:15:08 -0300

    Updates

---
+++ precache-manifest.9dbf47fb1d3482e98fe7a6d7eeac8d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a19aac8cb90808dda79385f105c3e6",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "d941a9f779901b83e987",
+    "url": "/facebook-clone/static/css/main.157c1be2.chunk.css"
+  },
+  {
+    "revision": "2aa6c5872dced71c68d0",
+    "url": "/facebook-clone/static/js/2.654eeb7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/facebook-clone/static/js/2.654eeb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d941a9f779901b83e987",
+    "url": "/facebook-clone/static/js/main.b37ff60d.chunk.js"
+  },
+  {
+    "revision": "8d9db31eeef904c1dc55",
+    "url": "/facebook-clone/static/js/runtime-main.389e76a3.js"
+  }
+]);
